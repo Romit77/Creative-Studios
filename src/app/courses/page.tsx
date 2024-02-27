7edf66066709292607573ac8738324b061815e09
Romit  Dey <romit77dey@gmail.com>
2024-02-27 21:06:01 +0530

    added hero section

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <div className="flex text-center items-center justify-center">
+        <h1 className="font-bold flex text-center justify-center items-center mt-[350px] text-3xl">
+          this page is under development
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default page;
