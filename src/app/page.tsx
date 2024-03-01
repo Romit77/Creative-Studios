05d26119c7f6500b7877bd667f67b519e3528c3e
Romit  Dey <romit77dey@gmail.com>
2024-03-01 13:18:58 +0530

    added testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import ChooseScroll from "@/components/ChooseScroll";
+import Cards from "@/components/Cards";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <ChooseScroll />
+      <Cards />
     </main>
   );
 }
