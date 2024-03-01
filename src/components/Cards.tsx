6c2e497b177a6a09164bf3419c2eff8e6e438269
Romit  Dey <romit77dey@gmail.com>
2024-03-01 21:51:00 +0530

    testimonial section completed

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -50,7 +50,8 @@ const testimonials = [
 
 function Cards() {
   return (
-    <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+    <div className="h-[40rem] w-full dark:bg-zinc dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <h2 className="text-3xl font-bold text-center mb-8 z-10">
         Hear our Harmony: Voices of success
       </h2>
